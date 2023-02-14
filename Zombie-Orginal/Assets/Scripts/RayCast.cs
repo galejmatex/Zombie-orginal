2a85da16d90eb36eeaff487bf4014ddfc93e03ff
Twenty <100616731+galejmatex@users.noreply.github.com>
2023-02-14 22:31:13 +0100

    test

---
+++ Zombie-Orginal/Assets/Scripts/RayCast.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+       if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, 20f ))
+        {
+            Debug.Log("Hit Somthing");{
+        }
+    }
+        else
+        {
+            Debug.Log("Hit Nothing");
+        }
+}
